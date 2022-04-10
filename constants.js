697f96a9c4077786550ffe97040c39906a6b245a
Giuseppe <peppeleocata@gmail.com>
2022-04-10 15:58:47 +0200

    updates

---
+++ constants.js
@@ -0,0 +1,38 @@
+﻿const RESULTS_MAP = {
+'medico': {
+title: 'Medico: "fai la fattura!!"',
+contents: "Il medico è il professionista della medicina che si occupa della salute umana e animale, prevenendo, diagnosticando e curando le malattie. Il termine è estensivo e comprende professionisti della salute umana, chiamati nel linguaggio colloquiale semplicemente medici, e professionisti della salute e del benessere animale, nonché della salute pubblica denominati medici veterinari (nel linguaggio corrente si sintetizza spesso la professione in \"veterinario\")..",
+},
+'matematico': {
+title: 'Matematico!',
+contents: "Un matematico è una persona che effettua studi, ricerche e sperimentazioni riguardanti problemi della matematica. Alcuni scienziati di altri campi di ricerca possono essere considerati matematici se la loro ricerca offre nuove idee matematiche; un esempio notevole è Edward Witten. Al contrario, alcuni matematici possono studiare problemi matematici relativi ad altri settori della ricerca scientifica e tecnologica (es. fisica matematica), queste persone sono conosciute come matematici applicati.",
+},
+'geometra': {
+title: 'Geometra: perfezionista',
+contents: "Il geometra (abbreviato geom.) è il professionista che misura, valuta e progetta opere pubbliche o private di modesta entità in muratura portante fino a 3 piani fuori terra. Può inoltre progettare opere di modesta entità in cemento armato. Il suo percorso di studi gli consente di stimare la proprietà privata, costruita o non, la superficie e il sottosuolo oltre che operare nel settore urbanistico, della direzione dei lavori e della sicurezza. La preparazione scolastica offre inoltre i primi approcci agli impianti idrici e fognari ed altri materiali da costruzione, calcestruzzo armato compreso. Il lavoro del geometra concerne prevalentemente i lavori di progettazione edilizia, impiantistica sui beni immobili. Ma il percorso di studi svolto lo prepara anche alla conoscenza dei diritti reali di godimento. La sua competenza è specialistica e comprende anche materie tecniche, topografiche, giuridiche, fiscali, economiche, beni agricoli e sociali.",
+},
+'politic': {
+title: 'Politico!',
+contents: "Si definisce politico o personalità politica sia chi detiene una carica negli organismi statali, a livello di governo centrale o ente territoriale, sia gli esponenti degli apparati dei partiti politici con responsabilità di rappresentanza e/o coordinamento e/o gestionali della macchina partitica.",
+},
+'chimico': {
+title: 'Chimico: una forza positiva nel mondo',
+contents: "Un chimico è uno scienziato che si occupa di chimica. I chimici sono impiegati nelle università come professori e ricercatori, nei laboratori di organismi pubblici di ricerca (tra quelli italiani ricordiamo l'INFN, l'ENEA, CNR e 'IIT, tra gli europei il CERN) e di certificazione (tra quelli italiani si ricordano l'ISS, tra quelli dell'Unione Europea l'EFSA e l'ECHA), nelle strutture ospedaliere in qualità di esperti per le applicazioni della chimica e biochimica in medicina o come specialisti dell'Alimentazione, nelle Agenzie Regionali per la Protezione dell'Ambiente (ARPA), nelle scuole secondarie, nei laboratori di analisi e ricerca industriali, nei laboratori di analisi privati.",
+},
+'nerd': {
+title: 'Fisico/Nerd: in breve, sarai "Sheldon Cooper" 2.0 di "The Big Bang Theory"!',
+contents: "Un fisico è uno scienziato che si occupa di fisica. I fisici sono impiegati nelle università come professori e ricercatori, e nei laboratori di enti di ricerca. L'occupazione come fisico professionista richiede la laurea magistrale; per la carriera presso le università e gli enti di ricerca pubblici è necessario il dottorato di ricerca."
+},
+'developer': {
+title: 'Software Developer: Mbare si u megghiu',
+contents: "In informatica, lo sviluppatore web (o in inglese web developer) è una figura professionale che si occupa dello sviluppo di applicazioni web, in particolare dello sviluppo del codice sorgente di programmazione, dell'ottimizzazione dei database e dei linguaggi di markup attraverso tecniche di programmazione Web.",
+},
+'data_science': {
+title: 'Data Scientist: prevederai il futuro',
+contents: "Un data scientist si occupa di sviluppare strategie per l'analisi dei dati, preparare i dati per l'analisi, esplorare, analizzare e visualizzare i dati, creare modelli con i dati utilizzando linguaggi di programmazione quali Python e R, nonché implementare i modelli nelle applicazioni.",
+},
+'economic': {
+title: 'Commercialista: più forti di quanto pensiate',
+contents: "Il commercialista redige il bilancio d'esercizio del cliente, gestisce le operazioni fiscali e previdenziali, e le relazioni con l'Agenzia delle Entrate. Inoltre prepara la dichiarazione dei redditi annuale per conto di società e privati, e la presenta all'amministrazione finanziaria (il Fisco).",
+}
+};
---
+++ script.js
@@ -0,0 +1,154 @@
+/*************************************  GLOBAL VARIABLES ************************************/
+const answers = document.querySelectorAll('.choice-grid div');
+
+let answerToFirstQuestion;
+let answerToSecondQuestion;
+let answerToThirdQuestion;
+
+/****************************************** FUNCTIONS ***************************************/
+function storeTheAnswer (answer) {
+    if (answer.dataset.questionId === 'one') answerToFirstQuestion = answer;
+    else if (answer.dataset.questionId === 'two') answerToSecondQuestion = answer;
+    else  answerToThirdQuestion = answer;
+}
+
+function isQuizFinished () {
+    return answerToFirstQuestion !== undefined &&
+            answerToSecondQuestion !== undefined &&
+            answerToThirdQuestion !== undefined;
+}
+
+function findElementWithHighestOccurrences(List) {
+    //accertiamoci prima che non sia vuota
+    if(List.length === 0)
+        return null;
+
+    let Map = {};
+    let maxEl = List[0]; // di default è la prima
+    let maxCount = 1;
+
+    for(let i = 0; i < List.length; i++) {
+        if (Map[List[i]] == null)
+            Map[List[i]] = 1;
+        else
+            Map[List[i]]++;
+
+        if(Map[List[i]] > maxCount)
+        {
+            maxEl = List[i];
+            maxCount = Map[List[i]];
+        }
+    }
+    return maxEl;
+}
+
+function showResultOfTheQuiz () {
+    const result = document.querySelector('#result');
+    result.classList.remove('hidden');
+
+    let idChoicedAnswers = [answerToFirstQuestion.dataset.choiceId,
+                            answerToSecondQuestion.dataset.choiceId,
+                            answerToThirdQuestion.dataset.choiceId];
+
+    let dominantAnswerId = findElementWithHighestOccurrences(idChoicedAnswers);
+
+    result.querySelector('h1').textContent = RESULTS_MAP[dominantAnswerId].title;
+    result.querySelector('p').textContent = RESULTS_MAP[dominantAnswerId].contents;
+
+    // aggiungiamo un EventListener al bottone 'Ricomincia Quiz'
+    const button = result.querySelector('button');
+    button.addEventListener('click', resetAll);
+
+    // scrolliamo fino in fondo
+    document.body.scrollTop = (document.documentElement.scrollTop = document.documentElement.scrollHeight);
+}
+
+function addEventListenerToAllAnswers () {
+    for (const answer of answers) {
+        answer.addEventListener('click', selectAnswer);
+    }
+}
+
+function removeEventListenerToAllAnswers () {
+    for (const answer of answers) {
+        answer.removeEventListener('click', selectAnswer);
+    }
+}
+
+function checkTheBoxOf (answer) {
+    const img = answer.querySelector('img.checkbox')
+    img.src = 'images/checked.png'
+}
+
+function uncheckTheBoxOf (answer) {
+    const img = answer.querySelector('img.checkbox')
+    img.src = 'images/unchecked.png'
+}
+
+function lightenTheOthersAnswersWithSameID (answer) {
+    for (const item of answers) {
+        if (item.dataset.questionId === answer.dataset.questionId && item !== answer)
+            item.classList.add('unselected');
+    }
+}
+
+function restoreallAnswersWithSameID (questionId) {
+    for (const item of answers) {
+        if (item.dataset.questionId === (questionId)) {
+            item.classList.remove('unselected');
+            item.classList.remove('selected');
+            uncheckTheBoxOf(item);
+        }
+    }
+}
+
+function restoreAllAnswers() {
+    for (const item of answers) {
+        item.classList.remove('unselected');
+        item.classList.remove('selected');
+        uncheckTheBoxOf(item);
+    }
+}
+
+function hideResultOfTheQuiz () {
+    const result = document.querySelector('#result');
+    result.classList.add('hidden');
+}
+
+/********************************************* HANDLERS *********************************************/
+function selectAnswer(event) {
+    const answer = event.currentTarget; // answer = elemento a cui è associato l'eventListner
+
+    restoreallAnswersWithSameID(answer.dataset.questionId);
+    answer.classList.add('selected');
+    checkTheBoxOf(answer);
+
+    storeTheAnswer(answer);
+
+    lightenTheOthersAnswersWithSameID(answer);
+        
+    if (isQuizFinished()) {
+        removeEventListenerToAllAnswers();
+        showResultOfTheQuiz();
+    }
+}
+
+function resetAll() {
+    // Le risposte scelte devono tornare al loro aspetto originario.
+    restoreAllAnswers();
+    // Il risultato sulla personalità deve scomparire.
+    hideResultOfTheQuiz();
+    // Le risposte devono poter essere nuovamente selezionabili.
+    addEventListenerToAllAnswers();
+    // La pagina deve sembrare e comportarsi come se aveste ricaricato la pagina => torniamo in alto
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+    //INIZIALIZZIAMO TUTTE LE VARIABILI GLOBALI
+    answerToFirstQuestion = undefined;
+    answerToSecondQuestion = undefined;
+    answerToThirdQuestion = undefined;
+}
+
+
+/***************************************** """MAIN""" *******************************************/
+addEventListenerToAllAnswers();
